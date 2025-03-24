15b22e9639ee4ffbffcc39b61b2f78e8f0538a0b
ccfreck <cafreckmann@calebPC>
2025-03-24 18:07:42 -0400

    Algorithm and File Choosing Added into the cmd line

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,63 @@
 #include "sudoku_solver.h"
 #include "timer.h"
+#include <iostream>
 
-// int main() 
-// {
-//     //timer stuff 
-//     Timer timer;
-
-//     vector<vector<int>> matrix(SIZE, vector<int>(SIZE, 0));
-//     readSudokuFromFile(matrix, "../input_files/input2.txt");
-//     printSudoku(matrix);
-    
-//     cout << "SOLVING MATRIX USING Sequential DFS\n";
-    
-//     // Call either the sequential or parallel solver
-//     timer.start();
-//     // double start = omp_get_wtime();
-//     // parallelDFSGateway(matrix);
-//     solveSudoku(matrix);
-//     auto tts = timer.getElapsedTime();
-
-//     std::cout << "execution time for sequential: " << tts << " ms\n" << endl;
-
-//     printSudoku(matrix);
-//     return 0;
-// }
+using namespace std;
 
 int main() {
-    SudokuGrid sudoku = {
-        {0, 0, 0, 0, 0, 0, 0, 1, 0}, 
-        {4, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 2, 0, 0, 0, 0, 0, 0, 0}, 
-        {0, 0, 0, 0, 5, 0, 4, 0, 7},
-        {0, 0, 8, 0, 0, 0, 3, 0, 0},
-        {0, 0, 1, 0, 9, 0, 0, 0, 0},
-        {3, 0, 0, 4, 0, 0, 2, 0, 0},
-        {0, 5, 0, 1, 0, 0, 0, 0, 0},
-        {0, 0, 0, 8, 0, 6, 0, 0, 0} 
-    };
+    int choice;
+    int fileChoice;
+    SudokuGrid sudoku(SIZE, vector<int>(SIZE, 0));
 
-    start = omp_get_wtime();
+    // Ask user to select algorithm type
+    cout << "Select solving method:\n";
+    cout << "1. Sequential DFS\n";
+    cout << "2. Parallel DFS\n";
+    cout << "Enter choice (1 or 2): ";
+    cin >> choice;
+
+    // Ask user to select input file (1-10)
+    cout << "Select input file (1-10): ";
+    cin >> fileChoice;
+
+    if (fileChoice < 1 || fileChoice > 10) {
+        cout << "Invalid file selection. Please choose between 1 and 10.\n";
+        return 1;
+    }
+
+    // Construct file path
+    string filePath = "../input_files/input" + to_string(fileChoice) + ".txt";
+    
+    // Read Sudoku puzzle from file
+    readSudokuFromFile(sudoku, filePath);
+
     cout << "Solving Sudoku:\n";
     printSudoku(sudoku);
     cout << "---------------------\n";
 
-    parallelDFSGateway(sudoku);
+    Timer timer;
+    timer.start();
+
+    if (choice == 1) 
+    {
+        cout << "<<<< Using Sequential DFS >>>>\n";
+        solveSudoku(sudoku);
+    } 
+    else if (choice == 2) 
+    {
+        cout << "<<<< Using Parallel DFS >>>>\n";
+        start = omp_get_wtime();
+        parallelDFSGateway(sudoku);
+    } 
+    else 
+    {
+        cout << "Invalid choice. Please enter 1 or 2.\n";
+        return 1;
+    }
+
+    auto elapsedTime = timer.getElapsedTime();
+    cout << "Execution time: " << elapsedTime << " ms\n";
+
+    printSudoku(sudoku);
+    return 0;
 }
